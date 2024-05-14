993002fff9a412032a596ce8899472e779602ec1
Fábio <fabiogoncalvesk2001@gmail.com>
2024-05-14 12:46:40 +0100

    make a domain and remove boilerplate from src folder

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {} //dependency injection
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+}
